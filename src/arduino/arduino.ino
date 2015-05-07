7deabcacb28eb555f9ec2da2ae8752fec51c11e1
Tobias Wilken <tooangel@tooangel.de>
2015-05-07 09:03:15 +0200

    Test the motor

--- src/arduino/arduino.ino
+++ src/arduino/arduino.ino
@@ -2,6 +2,7 @@ int sensorValue[] = {0, 0, 0, 0, 0, 0};
 int pin[] = {A0, A1, A2, A3, A4, A5};
 
 void setup() {
+	pinMode(12, OUTPUT);
 	Serial.begin(9600);
 	while(!Serial);
 }
@@ -13,5 +14,10 @@ void loop() {
 		Serial.print(": ");
 		Serial.println(sensorValue[i]);
 	}
+	digitalWrite(12, HIGH);
+	Serial.println("HIGH");
 	delay(10000);
+	digitalWrite(12, LOW);
+	Serial.println("LOW");
+	delay(1000);
 }
